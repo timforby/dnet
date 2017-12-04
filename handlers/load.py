5e30393f97bfe2221192253944a5520b1eb07923
Tim <timforby@gmail.com>
2017-12-04 11:50:47 -0500

    updating load and proc and adding gitignore

---
+++ handlers/load.py
@@ -0,0 +1,57 @@
+from scipy import misc
+import os
+import numpy as np
+
+# Walk through path and get file names as strings
+def get_filenames(path):
+    f_names = []
+    for (dir_path, dir_names, file_names) in os.walk(path):
+        f_names.extend(sorted(file_names))
+        break
+    return f_names
+
+def get_num(stri):
+    num = stri.split('.')[0]
+    return int(num)
+
+
+# Load images in given directory
+def load_imgs(dir_name, start, end, step):
+    fnames = get_filenames(path)
+    imgs = []
+    count = 0
+    for img in fnames:
+        if not img.endswith("png"):
+            continue
+        try:
+            if get_num(img) not in range(start,end,step):
+                continue
+        except ValueError:
+            print(img+": not read :: could not parse int")
+            continue
+        count += 1
+        fullpath = os.path.join(dir_name, img)
+        img = misc.imread(fullpath)
+        img = img/255.0
+        if len(img.shape) ==2:
+            img = np.reshape(img,(img.shape[0],img.shape[1],1))
+        imgs.append(img)
+    print()
+    return imgs
+
+
+def load_data(paths, start=0, end=999, step=1):
+    """Load images into a list
+    #Arguments
+        paths: List of strings representing paths to folders containing
+            images that must be named as numbers
+        start,end,step: Refers to the number of name of images. Only loads
+            images with in this range.
+    """
+    sets_imgs = []
+    for path in paths:
+        imgs = load_imgs(path,start,end,step)
+        sets_imgs.append(imgs)
+
+    return sets_imgs
+    
--- handlers/proc.py
+++ handlers/proc.py
@@ -1,15 +1,14 @@
 import numpy as np
-from sklearn.preprocessing import label_binarize as lb
 
 class Process:
 	@classmethod
 	def setup(cls, s_l, l):
-		'''
+		"""Sets up the labels for the network.
 		#Arguments
 			s_l: Selected labels to learn in network
 			l: labels that correspond to all labels in images when flattened to single channel.
 				This is required because some images might not have all labels. Example = Red,Blue,Yellow = 1,4,3
-		'''
+		"""
 		cls.selected_labels = s_l
 		cls.max_labels = l
 	
@@ -17,12 +16,18 @@ class Process:
 	def join_imgs(xs,ys,depth=1):
 		return [np.concatenate(x,y[:,:,:depth],axis=3) for x,y in zip(xs,ys)]
 
+	@staticmethod
+	def label_bin(y, max_labels):
+		_y = np.zeros((y.shape[0], max_labels),dtype=np.int)
+		_y[np.arange(y.shape[0]),y] = 1
+		return _y
+
 	@classmethod
 	def categorize_img(cls, y):
 		y = cls.to_single_channel(y)
 		_y = y.reshape((y.size,1))
 		_sz = len(cls.max_labels)
-		_y = lb(_y,range(_sz))
+		_y = label_bin(_y,range(_sz))
 		_y = _y.reshape((y.shape[:2]+(_sz,))).astype(int)
 		_y = _y[:,:,cls.selected_labels]
 		return _y
@@ -49,15 +54,40 @@ class Process:
 		y_train = []
 
 		while True:
+			#Randomizes all possible locations into "patch_locations"
 			patch_locations = []
-			max_options = 0
+			sum_locations = 0
+			all_locations = []
 			for y in ys:
-				options = int((y.shape[0]-patch_size[0])*(y.shape[1]-patch_size[1]))
-				max_options = max(max_options,options)
-				patch_locations.append(np.random.permutation(options))
+				locations = int((y.shape[0]-patch_size[0])*(y.shape[1]-patch_size[1]))
+				sum_locations += locations
+				all_locations.append(locations)
+				patch_locations.append(np.random.permutation(locations))
 			patch_index = [0]*len(ys)
 			
-			while
-				index=patch_locations[i][patch_index[i]%len(patch_locations[i])]
-				x_patch = getPatch(xs[i], index)
-				y_patch = getPatch(ys[i], index)
+			i = 0
+			while sum(patch_index) < sum_locations:
+				#If patch_index has not reached all locations
+				if patch_index[i] < all_locations[i]:
+					index=patch_locations[i][patch_index[i]]
+					x_patch = get_patch(xs[i], patch_size, index)
+					y_patch = get_patch(ys[i], patch_size, index)
+					x_train.append(x_patch)
+					y_train.append(y_patch)
+					patch_index[i] += 1
+				i+=1
+				i = i%len(y)
+
+				if len(y_train) == batch_size:
+					y_train = np.array(y_train)
+					x_train = np.array(x_train)
+					yield x_train, y_train
+					x_train = []
+					y_train = []
+		
+		
+	@staticmethod
+	def get_patch(img, patch_size, index):
+		x_val = index%(img.shape[0]-patch_size[0])
+		y_val = index//(img.shape[0]-patch_size[0])
+		return img[x_val:x_val+patch_size[0],y_val:y_val+patch_size[1],:]
