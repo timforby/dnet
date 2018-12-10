c22ddee90f4aef62905a2bdfd8fc6e8667bcd0f9
Tim (lab) <timforby@gmail.com>
2018-12-10 13:52:56 -0500

    wasserstein

--- urban_gan/train_new.py
+++ urban_gan/train_new.py
@@ -34,18 +34,27 @@ def binary_to_rgb(y):
     y = np.concatenate([y%2,(y//2)%2,(y//4)%2],axis=2)
     return y
 
+def clip_weights(model: nn.Module):
+    for p in model.parameters():
+        p.data.clamp_(-0.05, 0.05)
+
+    
 img_size = 144
 batch_size = 32
 
 print("Net setup")
 rgb_generator = unet(5,3)
-rgb_generator_opt = torch.optim.Adam(rgb_generator.parameters())
+#rgb_generator_opt = torch.optim.Adam(rgb_generator.parameters())
+rgb_generator_opt = torch.optim.RMSprop(rgb_generator.parameters())
 rgb_generator.cuda()
 
 disc = half_unet(3,1)
-disc_opt = torch.optim.Adam(disc.parameters())
-disc_criterion = nn.BCELoss()
+#disc_opt = torch.optim.Adam(disc.parameters())
+disc_opt = torch.optim.RMSprop(disc.parameters())
+disc_criterion = nn.L1Loss()
+#disc_criterion = nn.BCELoss()
 disc.cuda()
+clip_weights(disc)
 
 rgb_segment = unet(3,6)
 rgb_segment_opt = torch.optim.Adam(rgb_generator.parameters())
@@ -54,10 +63,11 @@ rgb_segment.cuda()
 
 print("Data setup")
 #rgb_wi_gt_data = loader(["test_image/rgb","test_image/y"],img_size,batch_size,transformations=[None,rgb_to_binary])
+#rgb_wi_gt_data = loader(["../../data/vaihingen/rgb","../../data/vaihingen/y"],img_size,batch_size,transformations=[None,rgb_to_binary])
 rgb_wi_gt_data = loader(["../../data/vaihingen/rgb","../../data/vaihingen/y"],img_size,batch_size,transformations=[lambda x: x-load.get_mean("../../data/vaihingen/rgb"),rgb_to_binary])
 data_wi_gt = rgb_wi_gt_data.generate_patch()
 
-
+#rgb_no_gt_data = loader(["../../data/test/rgb_ng"],img_size,batch_size,transformations=[])
 rgb_no_gt_data = loader(["../../data/test/rgb_ng"],img_size,batch_size,transformations=[lambda x: x-load.get_mean("../../data/test/rgb_ng")])
 data_no_gt = rgb_no_gt_data.generate_patch()
 fake_gt_data = loader(["../../data/vaihingen/y"],img_size,batch_size)
@@ -69,6 +79,9 @@ zero = torch.FloatTensor(batch_size).fill_(0).cuda()
 print("Running...")
 counter = 1
 for epoch in range(1000):
+    errDiscriminitor = []
+    errSegnet = []
+    errGenerator = []
     for (x,y),(z,),(fy,) in zip(data_wi_gt,data_no_gt,data_fake_gt):
         fy_cat = cat_batch(fy)
         y = y.transpose([0,3,1,2])
@@ -77,7 +90,7 @@ for epoch in range(1000):
         fy = fy.transpose([0,3,1,2])
         fy_cat = fy_cat.transpose([0,3,1,2])
         
-        print("counter: "+str(counter))
+        #print("counter: "+str(counter))
         counter += 1
         
         
@@ -93,22 +106,27 @@ for epoch in range(1000):
         rgb_gt_input = torch.from_numpy(y.squeeze(1)).cuda().long()
         #DISCRIMINATOR
         disc.zero_grad()
-        disc_ouput = disc(rgb_no_gt_input).view(-1, 1).squeeze(1) 
-        errD_real = disc_criterion(disc_ouput, ones[:disc_ouput.shape[0]])
+        #forward with desired distribution
+        disc_ouput = disc(rgb_no_gt_input).squeeze()
+        errD_real = disc_criterion(disc_ouput, ones)
         errD_real.backward()
-
-        disc_ouput = disc(fake_rgb.detach()).view(-1, 1).squeeze(1)
-        errD_fake = disc_criterion(disc_ouput, zero[:disc_ouput.shape[0]])
+        #forward with generated distribution
+        disc_ouput = disc(fake_rgb.detach()).squeeze()
+        errD_fake = disc_criterion(disc_ouput, zero)
         errD_fake.backward()
-
+        #optimize
         errD = errD_real + errD_fake
         disc_opt.step()
+        errDiscriminitor.append(errD.item())
 
+        clip_weights(disc)
+        
         #GENERATOR -> DISCRIMINATOR
         noise = torch.FloatTensor(batch_size, 2, img_size, img_size).normal_(0, 1)
         rgb_generator_input = torch.cat((fake_gt,noise),1).cuda()
 
         rgb_generator.zero_grad()
+        #forward with random + ground truth to generated distrib
         fake_rgb = rgb_generator(rgb_generator_input)
         disc_ouput = disc(fake_rgb).view(-1, 1).squeeze(1)
         errG_d = disc_criterion(disc_ouput, ones)
@@ -127,6 +145,7 @@ for epoch in range(1000):
         errS_fake.backward()
 
         errS = errS_real + errS_fake
+        errSegnet.append(errS.item())
         rgb_segment_opt.step()
 
         #GENERATOR -> SEGNET
@@ -139,9 +158,13 @@ for epoch in range(1000):
         errG_s.backward()
 
         errG = errG_d + errG_s
+        errGenerator.append(errG.item())  
         rgb_generator_opt.step()
 
-
+        if counter % 20 == 0:
+            print("Error discriminator: "+str(sum(errDiscriminitor)/len(errDiscriminitor))) 
+            print("Error segnet: "+str(sum(errSegnet)/len(errSegnet))) 
+            print("Error generator: "+str(sum(errGenerator)/len(errGenerator))) 
         if counter % 500 == 0:
-            vutils.save_image(fake_rgb,'%s/%03d_epoch_%s.png' % (".", counter, "fake_rgb"),normalize=False)
-            #vutils.save_image(fake_gt,'%s/%03d_epoch_%s.png' % (".", counter, "fake_gt"),normalize=False)
+            vutils.save_image(fake_rgb,'%s/%03d_epoch_%s.png' % (".", counter, "fake_rgb"),normalize=True)
+            vutils.save_image(fake_gt,'%s/%03d_epoch_%s.png' % (".", counter, "fake_gt"),normalize=False)
